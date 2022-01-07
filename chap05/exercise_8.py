d8b0d989161001afe8b2c4244369551da7e06c11
Dan <danielvediaj>
2022-01-07 18:20:36 +0100

    25% done

--- chap05/exercise_8.py
+++ chap05/exercise_8.py
@@ -1,19 +1,14 @@
 def main():
-
-#Get plaintext(p_text) and key(x) from the user
     p_text = input("Enter the message you'd like encrypted.\n")
-    key = eval(input("What's the key? : "))
-
+    p_text = p_text.replace(" ", "_")
     p_text = p_text.lower()
-    
-#Create string of letters
+ 
+    key = eval(input("What's the key? : "))
     table = "abcdefghijklmnopqrstuvwxyz"
 
-#Convert plaintext to ciphertext(c_text) using cipher loop
-    c_text = ""
-    for ch in p_text:
-        c_text = c_text + (table[((ord(ch)) - 97) + key % 52])
-
+    c_text = "".join(table[((ord(ch)) - 97) + (key % 52)] for ch in p_text)
     print("Your encoded message is {0}.".format(c_text))
 
 main()
+ord('_')
+
