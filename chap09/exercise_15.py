9c25ac28885710e24588852245779ee2e3a0dd3c
Dan <danielvediaj>
2022-01-28 18:14:03 +0100

    done 15 is wrong!!

--- chap09/exercise_15.py
+++ chap09/exercise_15.py
@@ -8,59 +8,53 @@
 
 #each wall takes up 60 degrees of vision from the center of the cube
 #1/6 is approximately 16.67%
-import math as m
+import math
 from random import random
 from graphics import *
 
 def main():
-    n = eval(input("How many should the simulation look towards a wall? "))
+    n = eval(input("How many steps close to the wall? "))
     prob1 = percentWall(n)
     num, den = percFraction(prob1)
     printSummary(num, den, prob1)
 
 def percentWall(n):
     wall1 = 0
-    for i in range (n):
+    for _ in range (n):
         if simOneLook() is True:
-            wall1 = wall1 + 1
+            wall1 += 1
         else:
             wall1 = wall1
-
-    prob1 = wall1 / n
-    return prob1
+    return wall1 / n
 
 def simOneLook():
-    #halfway towards wall1
-    x = 50
+    x = 0
     y = 0
-
-    angle = random() * 2 * m.pi
+    angle = random() * 2 * math.pi
     #travel to wall and stop
-    while (abs(x) <= 100) and (abs(y) <= 100):
-        x = x + m.cos(angle)
-        y = y + m.sin(angle)
-    if x >= 100:
-        return True
-    else:
-        return False
+    while (abs(x) <= 10) and (abs(y) <= 10):
+        x += math.cos(angle)
+        y += math.sin(angle)
+    return x >= 10
 
 def percFraction(prob1):
-    x = round(prob1, 2) * 100
-    m = 100
+    x = round(prob1, 2) * 10
+    m = 10
     n = x
     while m != 0:
         y = m
         m = n % m
         n = y
-
     num = x / n
-    den = 100 / n
+    den = 10 / n
 
     return num, den
-
 
 
 def printSummary(num, den, prob1):
     print("The closest wall occupies {0}/{1} of your vision, or {2:0.1%}.".format(int(num), int(den), prob1))
 
 if __name__ == '__main__': main()
+
+
+main()
